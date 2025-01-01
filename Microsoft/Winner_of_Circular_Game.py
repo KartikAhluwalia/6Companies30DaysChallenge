7c122d276582fa72800dcc5acffdc21bddc25aee
Kartik Ahluwalia <kartikahluwalia1304@gmail.com>
2025-01-02 00:07:46 +0530

    Day 2

---
+++ Microsoft/Winner_of_Circular_Game.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findTheWinner(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        q = deque()
+        for i in range(1,n+1):
+            q.append(i)
+
+        while len(q)>1:
+            for i in range(k-1):
+                num = q.popleft()
+                q.append(num)
+            q.popleft()
+            
+        return q[0]
+        
